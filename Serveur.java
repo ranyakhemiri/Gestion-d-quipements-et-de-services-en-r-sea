a9b2151044ae64a99d9581e09c719ad462866762
Rania Khemiri <rkhemiri.yestunisia2016@gmail.com>
2023-02-02 18:58:33 +0100

    classer Serveur complète

--- Serveur.java
+++ Serveur.java
@@ -1,3 +1,24 @@
+import java.nio.charset.MalformedInputException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+// Etape 1 : instancier l'implantation   
+// Etape 2 : rendre accessible les services RMI à distance 
+// à l'aide de Naming
+
 public class Serveur {
     
+    public static void main(String[] args) throws RemoteException, MalformedURLException{
+        // Etape 0 : lancer l'annuaire depuis le serveur directement 
+        // port choisi: 1090
+        LocateRegistry.createRegistry(1090);
+
+        //Etape 1: instancier l'imp
+        EquImpl obj = new EquImpl();
+
+        //Etape 2: naming 
+        Naming.rebind("rmi://localhost:1090/equipdape",obj);
+    }
+
 }
