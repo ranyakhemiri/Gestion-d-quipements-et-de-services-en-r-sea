b5c2c1c78d02b8f678655c3a02ad3944e417fcc8
ranyakhemiri <ranyakh2@gmail.com>
2023-02-20 13:23:20 +0100

    ajout de hashmap et mot de passe

--- EquImpl.java
+++ EquImpl.java
@@ -1,42 +1,55 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 
 // dans cette classe, on définit les méthodes abstraites déclarées dans l'interface "EquInter"
 
-public class EquImpl extends UnicastRemoteObject implements EquInter{
-    
+public class EquImpl extends UnicastRemoteObject implements EquInter {
+
     // on instancie un equipement "eq"
     private Equipement eq;
 
-    public EquImpl(Equipement eq) throws RemoteException{
+    // ******** BASE DE DONNEES ********
+    // on choisit de créer une HashMap pour l'equipement
+    // on peut aussir choisir de créer un fichier texte en guise de base de données
+    private HashMap<String, String> eqHash = new HashMap<>();
+
+    public EquImpl(Equipement eq) throws RemoteException {
         super();
-        this.eq= eq;
+        this.eq = eq;
+        eqHash.put("Name", eq.getName());
+        eqHash.put("Address", eq.getAddress());
+        eqHash.put("Service", eq.getService());
+        eqHash.put("Id", Integer.toString(eq.getId()));
     }
 
     // récupérer le nom de l'equipement
-    public String getName() throws RemoteException{
+    public String getName() throws RemoteException {
         return eq.getName();
     }
+
     // modifier le nom de l'equipement
-    public void setName(String name) throws RemoteException{
+    public void setName(String name) throws RemoteException {
         eq.setName(name);
     }
 
     // récupérer le nom du service
-    public String getService() throws RemoteException{
+    public String getService() throws RemoteException {
         return eq.getService();
     }
+
     // modifier le nom du service
-    public void setService(String s) throws RemoteException{
+    public void setService(String s) throws RemoteException {
         eq.setService(s);
     }
 
     // récupérer le nom du service
-    public String getAddress() throws RemoteException{
+    public String getAddress() throws RemoteException {
         return eq.getAddress();
     }
+
     // modifier le nom du service
-    public void setAddress(String ad) throws RemoteException{
+    public void setAddress(String ad) throws RemoteException {
         eq.setAddress(ad);
     }
 }
